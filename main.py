e305b902850b1f276a098b2e7417d98293cacb9a
Ryan Sturmer <ryansturmer@gmail.com>
2012-02-14 10:22:14 -0500

    Updated some stuff?  Maybe?  Git is confusing.

--- main.py
+++ main.py
@@ -3,29 +3,13 @@ import json
 import uuid
 from flask import Flask, render_template, request
 
-class Post(object):
-    def __init__(self, text):
-        self.uuid = uuid.uuid4().hex
-        self.timestamp = datetime.datetime.now()
-        self.text = text
+# MPD Stuff
+import mpd
 
-class Model(object):
-    def __init__(self):
-        self.posts = []
-        self.posts_by_uuid = {}
-    def add_post(self, post):
-        self.posts.append(post)
-        self.posts_by_uuid[post.uuid] = post
-    def get_posts_since(self, uuid):
-        if uuid in self.posts_by_uuid:
-            ref = self.posts_by_uuid[uuid]
-            posts = [post for post in self.posts if post.timestamp > ref.timestamp]
-        else:
-            posts = self.posts
-        return sorted(posts, cmp=cmp_timestamp, reverse=True)
+HOST = 'carmen'
+PORT = '6600'
 
-MODEL = Model()
-
+MODEL = mpd.MPDClient(HOST, PORT)
 # Flask Code
 app = Flask(__name__)
 
@@ -33,24 +17,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/_posts')
-def _posts():
-    uuid = request.args['since']
-    posts = MODEL.get_posts_since(uuid)
-    html = render_template('posts.html', posts=posts)
-    since = posts[0].uuid if posts else ''
-    return json.dumps({
-        'html': html, 
-        'since': since,
-        'count': len(posts),
-    })
+@app.route('/play', methods=['POST'])
+def play():
+    print "Playing thing..."
+    MODEL.play()
+    return ''
 
-@app.route('/_add_post', methods=['POST'])
-def _add_post():
-    text = request.form['text']
-    if text:
-        post = Post(text)
-        MODEL.add_post(post)
+@app.route('/stop', methods=['POST'])
+def stop():
+    MODEL.stop()
+    return ''
+
+@app.route('/next', methods=['POST'])
+def next():
+    MODEL.next()
+    return ''
+
+@app.route('/prev', methods=['POST'])
+def prev():
+    MODEL.previous()
     return ''
 
 if __name__ == '__main__':
